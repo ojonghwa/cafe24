8fedf6e75ed835e6768bfe0d5268df2ca3dc0d2d
ojonghwa <ojonghwa@naver.com>
2021-10-07 01:16:15 +0900

    key settings

--- api/views.py
+++ api/views.py
@@ -17,6 +17,7 @@ from .serializers import ProductSerializer, CategorySerializer, CategoryProductS
     OrderSerializer, OrderItemSerializer, PostSerializer, UserSerializer
 from django.db.models import Q
 import sys, requests
+from cafe24.settings import KoreaApiKey2
 
 #from rest_framework.decorators import api_view
 from iot.models import Esp8266
@@ -30,13 +31,12 @@ class getDustData(APIView):
 
     def get(self, request, format=None):
 
-        KoreaApiKey2 = "V7gnr5NtRL3R7HGoKR7Ry2J4cEptcZeGEu69y%2BTZ%2BGON4xh7pqYJTSELRGiHHsly95JbVj1M7G7tUyo6a%2BvVpw%3D%3D"
+        
         stationName  = "수지"
         #1일 100번까지 가능하므로 20분 간격으로 call 하도록 수정하고, 결과값을 변수에 담아두고 그 사이 호출 발생시 변수값을 반환 
 
         servername   = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?numOfRows=1&dataTerm=DAILY&ver=1.3&serviceKey=" + KoreaApiKey2 + "&stationName=" + stationName
-        #http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?numOfRows=1&dataTerm=DAILY&ver=1.3&serviceKey=V7gnr5NtRL3R7HGoKR7Ry2J4cEptcZeGEu69y%2BTZ%2BGON4xh7pqYJTSELRGiHHsly95JbVj1M7G7tUyo6a%2BvVpw%3D%3D&stationName=수지
-        
+  
         xmlString = requests.get(servername)
         #print(xmlString.text)
 
