eb79dbb6519ebd312d3e7b5fa95f374b12086097
ojonghwa <ojonghwa@naver.com>
2022-09-12 19:29:56 +0900

    book 추가

--- api/serializers.py
+++ api/serializers.py
@@ -8,57 +8,82 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ["username", "email"]
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name']
-        
+        fields = ["id", "name"]
+
 
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
-    likers  = serializers.StringRelatedField(many=True)
+    likers = serializers.StringRelatedField(many=True)
+
     class Meta:
         model = Product
-        fields = ['id', 'name', 'slug', 'price', 'category', 'image', 'publication_date',  
-            'description', 'contributors', 'publisher', 'isbn', 'likers' ]
+        fields = [
+            "id",
+            "name",
+            "slug",
+            "price",
+            "category",
+            "image",
+            "publication_date",
+            "description",
+            "contributors",
+            "publisher",
+            "isbn",
+            "likers",
+        ]
 
 
 class CategoryProductSerializer(serializers.ModelSerializer):
     products = ProductSerializer(many=True, read_only=True)
+
     class Meta:
         model = Category
-        fields = ['name', 'products']
+        fields = ["name", "products"]
 
 
 class ItemtSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['name']
+        fields = ["name"]
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
-    product = ItemtSerializer()  
+    product = ItemtSerializer()
+
     class Meta:
         model = OrderItem
-        fields = ['order', 'product', 'price', 'quantity']  
+        fields = ["order", "product", "price", "quantity"]
 
-        
+
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True)
+
     class Meta:
         model = Order
-        #fields = ['fullname', 'mobile', 'created', 'get_total_cost', 'items']
-        fields = '__all__'
+        # fields = ['fullname', 'mobile', 'created', 'get_total_cost', 'items']
+        fields = "__all__"
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
     product = ItemtSerializer(read_only=True)
-    voter  = serializers.StringRelatedField(many=True)
+    voter = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = Post
-        fields = ['id','title','body','product','author','created','status','voter']
+        fields = [
+            "id",
+            "title",
+            "body",
+            "product",
+            "author",
+            "created",
+            "status",
+            "voter",
+        ]
--- api/urls.py
+++ api/urls.py
@@ -2,36 +2,35 @@ from rest_framework import routers
 from django.urls import path, include
 from . import views
 
-app_name = 'shop_api'       
+app_name = "shop_api"
 
 router = routers.DefaultRouter()
 
-    #http://127.0.0.1:8000/api/category/",
-    #http://127.0.0.1:8000/api/category/1/",
+# http://127.0.0.1:8000/api/category/",
+# http://127.0.0.1:8000/api/category/1/",
 
-router.register('category', views.CategoryViewSet)   
-router.register('product', views.ProductViewSet)   
-router.register('post', views.PostViewSet) 
-router.register('user', views.UserViewSet) 
-router.register('order', views.OrderViewSet) 
-#router.register('category-product', views.CategoryProductViewSet)  위의 것과 동일함
+router.register("category", views.CategoryViewSet)
+router.register("product", views.ProductViewSet)
+router.register("post", views.PostViewSet)
+router.register("user", views.UserViewSet)
+router.register("order", views.OrderViewSet)
 
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('login/', views.Login.as_view(), name='login'),
-
-    #path('category/', views.CategoryListView.as_view(), name='category_list'),
-    #path('category/<pk>/', views.CategoryProductView.as_view(), name='product_list'),
-    #path('product/<pk>/', views.ProductDetailView.as_view(), name='product_detail'),
-    #path('order/<pk>/', views.OrderDetailView.as_view(), name='order_detail'),
-
-    path('orderitem/<pk>/', views.OrderItemDetailView.as_view(), name='orderitem_detail'),
-    path('orderlist/<username>/', views.OrderListDetailView.as_view(), name='order_list'),
-
-    path('iot/', views.iot_esp8266View.as_view()),
-    path('getDustData/', views.getDustData.as_view()),
-    path('getCoronaData/', views.getCoronaData.as_view()),
-    path('getWeatherData/<city>/', views.getWeatherData.as_view()),
+    path("", include(router.urls)),
+    path("login/", views.Login.as_view(), name="login"),
+    # path('category/', views.CategoryListView.as_view(), name='category_list'),
+    # path('category/<pk>/', views.CategoryProductView.as_view(), name='product_list'),
+    # path('product/<pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+    # path('order/<pk>/', views.OrderDetailView.as_view(), name='order_detail'),
+    path(
+        "orderitem/<pk>/", views.OrderItemDetailView.as_view(), name="orderitem_detail"
+    ),
+    path(
+        "orderlist/<username>/", views.OrderListDetailView.as_view(), name="order_list"
+    ),
+    path("iot/", views.iot_esp8266View.as_view()),
+    path("getDustData/", views.getDustData.as_view()),
+    path("getCoronaData/", views.getCoronaData.as_view()),
+    path("getWeatherData/<city>/", views.getWeatherData.as_view()),
 ]
-
