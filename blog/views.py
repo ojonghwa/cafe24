3ff0872b35f89acc8723e00095b6463f717b86cf
ojonghwa <ojonghwa@naver.com>
2023-07-17 06:14:23 +0900

    책추가

--- blog/views.py
+++ blog/views.py
@@ -13,124 +13,130 @@ import sys
 
 
 def index(request):
-    page = request.GET.get('page','1')      # 기본 페이지 1, ?page=1
-    kw = request.GET.get('kw', '')          # 검색어
-    so = request.GET.get('so', 'recent')    # 정렬기준
-    #print('kw=' + kw, file=sys.stdout)
+    page = request.GET.get("page", "1")  # 기본 페이지 1, ?page=1
+    kw = request.GET.get("kw", "")  # 검색어
+    so = request.GET.get("so", "recent")  # 정렬기준
+    # print('kw=' + kw, file=sys.stdout)
 
-    if so == 'recommend':       # 정렬
-        post_list = Post.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-created')
-    else:       # recent
-        post_list = Post.objects.order_by('-created')
+    if so == "recommend":  # 정렬
+        post_list = Post.objects.annotate(num_voter=Count("voter")).order_by(
+            "-num_voter", "-created"
+        )
+    else:  # recent
+        post_list = Post.objects.order_by("-created")
 
-    if kw:      # 검색
+    if kw:  # 검색
         post_list = post_list.filter(
-            Q(title__icontains=kw) |        # 제목검색
-            Q(body__icontains=kw)  |        # 내용검색
-            Q(author__username__icontains=kw)     # 작성자검색
+            Q(title__icontains=kw)
+            | Q(body__icontains=kw)  # 제목검색
+            | Q(author__username__icontains=kw)  # 내용검색  # 작성자검색
         ).distinct()
 
-    paginator = Paginator(post_list, 7)     # 페이지당 7개 표시 
+    paginator = Paginator(post_list, 7)  # 페이지당 7개 표시
     page_object = paginator.get_page(page)
 
-    products = Product.objects.order_by('name')
-    if kw:      # 검색
+    products = Product.objects.order_by("name")
+    if kw:  # 검색
         products = products.filter(
-            Q(name__icontains=kw) |         # 제목검색
-            Q(description__icontains=kw)    # 내용검색
+            Q(name__icontains=kw) | Q(description__icontains=kw)  # 제목검색  # 내용검색
         ).distinct()
     else:
         products = None
 
-    return render(request, 'blog/post_list.html', 
-        { 'post_list': page_object, 'page': page, 'kw': kw, 'so': so, 'products': products })
-
+    return render(
+        request,
+        "blog/post_list.html",
+        {
+            "post_list": page_object,
+            "page": page,
+            "kw": kw,
+            "so": so,
+            "products": products,
+        },
+    )
 
 
 def detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
-    return render(request, 'blog/post_detail.html', { 'post': post, 'product': post.product })
+    return render(
+        request, "blog/post_detail.html", {"post": post, "product": post.product}
+    )
 
 
-
-@login_required(login_url='common:login')
+@login_required(login_url="common:login")
 def post_create(request, product_id):
     product = get_object_or_404(Product, id=product_id)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user      # post 작성자 id 추가 
+            post.author = request.user  # post 작성자 id 추가
             post.save()
-            return redirect('blog:index')
+            return redirect("blog:index")
     else:
         form = PostForm()
-    return render(request, 'blog/post_form.html', {'form':form, 'product':product })
+    return render(request, "blog/post_form.html", {"form": form, "product": product})
 
 
-
-@login_required(login_url='common:login')
+@login_required(login_url="common:login")
 def post_modify(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     product = get_object_or_404(Product, id=post.product.id)
-    #print(post, file=sys.stdout)
+    # print(post, file=sys.stdout)
 
     if request.user != post.author:
-        messages.error(request, '수정권한이 없습니다.')
-        return redirect('blog:detail', post_id=post.id)
+        messages.error(request, "수정권한이 없습니다.")
+        return redirect("blog:detail", post_id=post.id)
 
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
-            post.updated = timezone.now()       # 수정일시
+            post.updated = timezone.now()  # 수정일시
             post.save()
-            return redirect('blog:detail', post_id=post.id)
+            return redirect("blog:detail", post_id=post.id)
     else:
         form = PostForm(instance=post)
 
-    return render(request, 'blog/post_form.html', {'form': form, 'product':product })
+    return render(request, "blog/post_form.html", {"form": form, "product": product})
 
 
-
-@login_required(login_url='common:login')
+@login_required(login_url="common:login")
 def post_delete(request, post_id):
     post = get_object_or_404(Post, id=post_id)
 
     if request.user != post.author:
-        messages.error(request, '삭제권한이 없습니다')
-        return redirect('blog:detail', post_id=post.id)
+        messages.error(request, "삭제권한이 없습니다")
+        return redirect("blog:detail", post_id=post.id)
 
     post.delete()
-    return redirect('blog:index')
-
+    return redirect("blog:index")
 
 
 @login_required()
 @require_POST
 def post_vote(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    action = request.POST.get('action')
+    action = request.POST.get("action")
 
     if request.is_ajax:
         print("Ajax action: " + action, file=sys.stdout)
 
     if post and action:
         try:
-            if action == 'vote':
-                post.voter.add(request.user)        # 추천
-                #request.user.voter_post.add(post)  # 위와 동일함
+            if action == "vote":
+                post.voter.add(request.user)  # 추천
+                # request.user.voter_post.add(post)  # 위와 동일함
             else:
-                post.voter.remove(request.user)     # 추천 취소
-                #request.user.voter_post.remove(post)
-            return JsonResponse({'status':'ok'})
+                post.voter.remove(request.user)  # 추천 취소
+                # request.user.voter_post.remove(post)
+            return JsonResponse({"status": "ok"})
         except:
             pass
-    return JsonResponse({'status':'error'})
+    return JsonResponse({"status": "error"})
 
 
-
-@login_required(login_url='common:login')
+@login_required(login_url="common:login")
 def comment_create(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
 
@@ -141,20 +147,22 @@ def comment_create(request, post_id):
             comment.author = request.user
             comment.post = post
             comment.save()
-            return redirect('{}#comment_{}'.format(
-                resolve_url('blog:detail', post_id=comment.post.id), comment.id))   # 스크롤 처리 앵커
+            return redirect(
+                "{}#comment_{}".format(
+                    resolve_url("blog:detail", post_id=comment.post.id), comment.id
+                )
+            )  # 스크롤 처리 앵커
     else:
         form = CommentForm()
-    return render(request, 'blog/comment_form.html', {'form': form})
+    return render(request, "blog/comment_form.html", {"form": form})
 
 
-
-@login_required(login_url='common:login')
+@login_required(login_url="common:login")
 def comment_modify(request, comment_id):
     comment = get_object_or_404(Comment, pk=comment_id)
     if request.user != comment.author:
-        messages.error(request, '댓글 수정권한이 없습니다')
-        return redirect('blog:detail', post_id=comment.post.id)
+        messages.error(request, "댓글 수정권한이 없습니다")
+        return redirect("blog:detail", post_id=comment.post.id)
 
     if request.method == "POST":
         form = CommentForm(request.POST, instance=comment)
@@ -163,21 +171,22 @@ def comment_modify(request, comment_id):
             comment.author = request.user
             comment.updated = timezone.now()
             comment.save()
-            return redirect('{}#comment_{}'.format(
-                resolve_url('blog:detail', post_id=comment.post.id), comment.id))
+            return redirect(
+                "{}#comment_{}".format(
+                    resolve_url("blog:detail", post_id=comment.post.id), comment.id
+                )
+            )
     else:
         form = CommentForm(instance=comment)
-    return render(request, 'blog/comment_form.html', {'form': form})
+    return render(request, "blog/comment_form.html", {"form": form})
 
 
-
-@login_required(login_url='common:login')
+@login_required(login_url="common:login")
 def comment_delete(request, comment_id):
     comment = get_object_or_404(Comment, pk=comment_id)
     if request.user != comment.author:
-        messages.error(request, '댓글 삭제권한이 없습니다')
-        return redirect('blog:detail', post_id=comment.post_id)
+        messages.error(request, "댓글 삭제권한이 없습니다")
+        return redirect("blog:detail", post_id=comment.post_id)
     else:
         comment.delete()
-    return redirect('blog:detail', post_id=comment.post_id)
-
+    return redirect("blog:detail", post_id=comment.post_id)
--- shop/recommender.py
+++ shop/recommender.py
@@ -2,46 +2,54 @@ from django.shortcuts import get_object_or_404
 from orders.models import OrderItem
 from .models import Product
 from pandas import DataFrame
-#import sys
+import sys
 
 
 def get_recommendations(id, max_results=3):
     orders_for_product = OrderItem.objects.filter(product_id=id)
-    #<QuerySet [<OrderItem: 19>, <OrderItem: 22>, <OrderItem: 23>]>
+    # <QuerySet [<OrderItem: 19>, <OrderItem: 22>, <OrderItem: 23>]>
+
+    # orders_querysetlist = list(OrderItem.objects.filter(product_id=id))
+    # print(orders_querysetlist[1].product, file=sys.stdout)
 
     order_ids = [p.order_id for p in orders_for_product]
-    #[13, 14, 15]
+    # [13, 14, 15]
 
     order_ids = OrderItem.objects.filter(order_id__in=order_ids)
-    #<QuerySet [<OrderItem: 19>, <OrderItem: 20>, <OrderItem: 21>, <OrderItem: 22>]>
+    # <QuerySet [<OrderItem: 19>, <OrderItem: 20>, <OrderItem: 21>, <OrderItem: 22>]>
 
     product_ids = [p.product_id for p in order_ids if p.product_id != id]
-    #[11, 15, 15, 11]
+    # [11, 15, 15, 11]
 
     product_ids.sort()
-    #[11, 11, 15, 15]
+    # [11, 11, 15, 15]
     num_orders_for_product = len(product_ids)
-    #4
+    # 4
 
     product_list = []
     [product_list.append(i) for i in product_ids if i not in product_list]
-    #[11, 15]
+    # [11, 15]
 
     count_list = []
-    [count_list.append(product_ids.count(i)/num_orders_for_product) for i in product_list]
-    #[0.5, 0.5]
+    [
+        count_list.append(product_ids.count(i) / num_orders_for_product)
+        for i in product_list
+    ]
+    # [0.5, 0.5]
 
-    raw_data = {'product_id': product_list, 'frequency': count_list }
+    raw_data = {"product_id": product_list, "frequency": count_list}
     dataframe = DataFrame(raw_data)
-    dataframe = DataFrame(dataframe.sort_values("frequency", ascending=False).head(max_results))
+    dataframe = DataFrame(
+        dataframe.sort_values("frequency", ascending=False).head(max_results)
+    )
     #   product_id  frequency
-    #0          11        0.5
-    #1          15        0.5
+    # 0          11        0.5
+    # 1          15        0.5
 
     recommends_ids = dataframe.product_id.tolist()
-    #print(recommends_ids, file=sys.stdout)
-    #[11, 15]
+    # print(recommends_ids, file=sys.stdout)
+    # [11, 15]
 
     product_ids = Product.objects.filter(id__in=recommends_ids)
-    
+
     return product_ids
